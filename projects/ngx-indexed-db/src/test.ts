5b28c2453ef8ebb0f4a0be6995e97fee6874d989
Charles Assunção <charlesassuncao@charless-macbook-pro.home>
2019-09-22 16:04:50 +0200

    chore: configure commitzen

--- projects/ngx-indexed-db/src/test.ts
+++ projects/ngx-indexed-db/src/test.ts
@@ -6,14 +6,10 @@ import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 
-
 declare const require: any;
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
