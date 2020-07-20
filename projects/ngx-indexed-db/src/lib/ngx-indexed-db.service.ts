c20e42c8b21b2d954aa85b0cd9135b852845215c
Dirk Rusche <dirk@rusche.me>
2020-07-20 16:23:23 +0200

    Fix wrong catch statement

--- projects/ngx-indexed-db/src/lib/ngx-indexed-db.service.ts
+++ projects/ngx-indexed-db/src/lib/ngx-indexed-db.service.ts
@@ -172,14 +172,17 @@ export class NgxIndexedDBService {
 
 	deleteDatabase() {
 		return new Promise(async (resolve, reject) => {
-			const db = await openDatabase(this.indexedDB, this.dbConfig.name, this.dbConfig.version);
-			await db.close();
-			const deleteDBRequest = this.indexedDB.deleteDatabase(this.dbConfig.name).catch(reason => reject(reason));
-			deleteDBRequest.onsuccess = resolve;
-			deleteDBRequest.onerror = reject;
-			deleteDBRequest.onblocked = () => {
-				throw new Error("Unable to delete database because it's blocked");
-			};
+			openDatabase(this.indexedDB, this.dbConfig.name, this.dbConfig.version)
+			.then(db => {
+				db.close();
+				const deleteDBRequest = this.indexedDB.deleteDatabase(this.dbConfig.name);
+				deleteDBRequest.onsuccess = resolve;
+				deleteDBRequest.onerror = reject;
+				deleteDBRequest.onblocked = () => {
+					throw new Error("Unable to delete database because it's blocked");
+				};
+			})
+			.catch(reason => reject(reason));
 		});
 	}
 
