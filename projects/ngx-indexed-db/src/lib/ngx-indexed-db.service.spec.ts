ab836ee51ef8e81ae86ed3da6a2e1bb771674b10
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-10-31 10:11:38 -0300

    fix(test): green initial tests

--- projects/ngx-indexed-db/src/lib/ngx-indexed-db.service.spec.ts
+++ projects/ngx-indexed-db/src/lib/ngx-indexed-db.service.spec.ts
@@ -1,9 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NgxIndexedDBService } from './ngx-indexed-db.service';
+import { NgxIndexedDBModule } from './ngxindexeddb.module';
+
+const dbConfig = {
+  name: 'MyDb',
+  version: 1,
+  objectStoresMeta: [
+    {
+      store: 'people',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'name', keypath: 'name', options: { unique: false } },
+        { name: 'email', keypath: 'email', options: { unique: false } },
+      ],
+    },
+  ],
+};
 
 describe('NgxIndexedDBService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [NgxIndexedDBModule.forRoot(dbConfig)],
+    })
+  );
 
   it('should be created', () => {
     const service: NgxIndexedDBService = TestBed.get(NgxIndexedDBService);
