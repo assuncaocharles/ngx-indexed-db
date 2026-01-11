0496b0eacabd7f0a8abd12030b57c124342d515f
Filip Zukal <130696213+sy-filis@users.noreply.github.com>
2026-01-11 19:50:54 +0100

    update: ng update

--- projects/playground/src/main.ts
+++ projects/playground/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { environment } from './environments/environment';
@@ -9,4 +9,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [provideZoneChangeDetection(), ...appConfig.providers],
+}).catch((err) => console.error(err));
